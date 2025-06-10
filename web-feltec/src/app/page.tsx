df9a3bbd60bde6e9563bc218f25c0282d9f3f106
Felipe-Camargo12 <felipeca1268@gmail.com>
2025-06-10 00:10:14 -0300

    feat: Base do website

---
+++ web-feltec/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <div>
+          <Header />
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        ISSO VAI SER O FOOTER
+      </footer>
+    </div>
+  );
+}
---
+++ web-feltec/src/components/header/index.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { Search, Bell, Info, ChevronDown, ChevronUp } from "lucide-react";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { useCallback, useState } from "react";
+import Image from "next/image";
+import Logo from "../../../public/Logo Feltec.png";
+
+export default function Header() {
+  return (
+    <header className="w-full h-16 bg-nc-base-600 text-white shadow bg-feltec-primary-dark">
+      <div className="w-full h-full grid grid-cols-4 items-center justify-between ">
+        <span className=" w-40 h-10 flex items-center justify-center font-bold text-2xl">
+          <Image
+            src={Logo}
+            alt="Feltec Logo"
+            className="w-full h-full object-contain"
+          />
+          FELTEC
+        </span>
+
+        <div className="flex items-center gap-2 ml-auto col-span-2">
+          <Button className="p-2 rounded bg-nc-base-800 hover:bg-nc-base-400">
+            <Info />
+          </Button>
+          <Button className="p-2 rounded bg-nc-base-800 hover:bg-nc-base-400">
+            <Search />
+          </Button>
+          <Button className="p-2 rounded bg-nc-base-800 hover:bg-nc-base-400">
+            <Bell />
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
