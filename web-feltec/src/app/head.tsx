acad2bec915a711109dd10ab4b4004e8c9a80294
Gabriel Oliveira <77246516+gabrieldeoliveira04@users.noreply.github.com>
2025-07-29 23:13:06 -0300

    Create head.tsx
    criação do head para carregamento da tag de verificação do google no html raiz do código
    
    Signed-off-by: Gabriel Oliveira <77246516+gabrieldeoliveira04@users.noreply.github.com>

---
+++ web-feltec/src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Feltec Solutions IT</title>
+      <meta name="description" content="Soluções de TI para empresas: sites, sistemas e apps sob medida. Atendemos São Paulo e todo o Brasil." />
+      <meta name="google-site-verification" content="nrxnEojxOirKFaPWsRCH6cbOwnMzVcuH6cgnvGT2pfU" />
+    </>
+  );
+}
