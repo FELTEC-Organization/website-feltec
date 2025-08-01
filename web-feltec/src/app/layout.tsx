1fada5f90af45a359a7a0b59f8ffab7420b02f80
Gabriel Oliveira <77246516+gabrieldeoliveira04@users.noreply.github.com>
2025-08-01 01:16:00 +0000

    adicionando o ícone favicon como imagem ao pesquisar a url no google

--- web-feltec/src/app/layout.tsx
+++ web-feltec/src/app/layout.tsx
@@ -18,6 +18,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Feltec - Soluções em TI",
   description: "Feltec website",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
